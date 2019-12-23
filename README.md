43e5095d33aa927c2e90e8c92a5f5a3f856a4ab5
Esperanza Cutillas Rastoll <especr101@gmail.com>
2019-12-23 20:44:59 +0100

    Se muestran los resultados  de la búsqueda en forma de tarjeta.

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Commands for creating the project:
 `conda create --name codecourseslist-app python=3`
 
 `django-admin startproject codecourseslist-app`
+
+To add style and stuff I have added these lines to the file base.html:
+
+```html
+<link
+  rel="stylesheet"
+  type="text/css"
+  media="screen"
+  href='{% static "css/style.css" %}'
+/>
+<!-- Compiled and minified CSS -->
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+/>
+<link
+  href="https://fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap"
+  rel="stylesheet"
+/>
+<link
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet"
+/>
+```
--- codecourseslist/codecoursesapp/views.py
+++ codecourseslist/codecoursesapp/views.py
@@ -1,5 +1,46 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus  # nos ayuda con las url si hay espacios
+from . import models
 
-# Create your views here.
+BASE_CRAIGSLIST_URL = 'https://alicante.craigslist.org/search/bbb?query='
+# BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/bbb?query='
+
+
 def home(request):
     return render(request, template_name='base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    # We save a search into ddbb
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL + "{}".format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    # Here we get the information
+    post_rows = soup.find_all('li', {'class': 'result-row'})
+
+    final_results = []
+    if len(post_rows) > 0:
+        for post in post_rows:
+            post_title = post.find(class_='result-title').text
+            post_url = post.find('a').get('href')
+        # post_title = post_rows.find_all('a', { 'class': 'result-title'})
+        # post_url = post_rows.find_all('p', { 'class': 'result-info'})
+        # post_price =
+        final_results.append({
+            'post_title': post_title,
+            'post_url': post_url
+        })
+
+    print(final_results)
+    data = {
+        'search': search,
+        'final_results': final_results
+    }
+    print(data)
+    return render(request, 'codecoursesapp/new_search.html', data)
