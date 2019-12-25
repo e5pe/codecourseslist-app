574963596f132db00c49050e0166fad83a800738
Esperanza Cutillas Rastoll <especr101@gmail.com>
2019-12-25 22:02:42 +0100

    Funcionalidad básica del buscador terminada.

--- codecourseslist/codecoursesapp/views.py
+++ codecourseslist/codecoursesapp/views.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 from requests.compat import quote_plus  # nos ayuda con las url si hay espacios
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://alicante.craigslist.org/search/bbb?query='
-# BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/bbb?query='
+# BASE_CRAIGSLIST_URL = 'https://alicante.craigslist.org/search/bbb?query='
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/bbb?query='
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -29,18 +30,27 @@ def new_search(request):
         for post in post_rows:
             post_title = post.find(class_='result-title').text
             post_url = post.find('a').get('href')
-        # post_title = post_rows.find_all('a', { 'class': 'result-title'})
-        # post_url = post_rows.find_all('p', { 'class': 'result-info'})
-        # post_price =
-        final_results.append({
-            'post_title': post_title,
-            'post_url': post_url
-        })
+            if post.find(class_='result-price'):
+                post_price = post.find(class_='result-price').text
+            else:
+                post_price = 'N/A'
 
-    print(final_results)
+            if post.find('a') and post.find('a').get('data-ids'):
+                ids = post.find('a').get('data-ids')
+                if len(ids) >= 1:
+                    id_image = ids.split(',')[0][2:]
+                    post_image_url = BASE_IMAGE_URL.format(id_image)
+                    print(post_image_url)
+            else:
+                post_image_url = 'https://craigslist.org/images/peace.jpg'
+            final_results.append({
+                'post_title': post_title,
+                'post_url': post_url,
+                'post_price': post_price,
+                'post_image_url': post_image_url
+            })
     data = {
-        'search': search,
+        'search': search, # What user has typed
         'final_results': final_results
     }
-    print(data)
     return render(request, 'codecoursesapp/new_search.html', data)
